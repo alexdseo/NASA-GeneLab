be9709dcabd7076a57cb67afb6c88e7e5b1f87f2
deepdong <donghyunmon@gmail.com>
2021-01-27 04:03:47 -0600

    additional data

--- Additional_Data/log_GLDS120.R
+++ Additional_Data/log_GLDS120.R
@@ -2,7 +2,7 @@
 set.seed(2021)
 nasa<-read.csv("GLDS-120_rna_seq_differential_expression.csv")
 #View(nasa)
-logfc_GLDS120<-nasa[,c(1,44:439)]
-log<-seq(2,397,3)
-logfc_GLDS120<-logfc_GLDS120[,c(1,log)]
+logfc_GLDS120<-nasa[,c(1:7,44:439)]
+log<-seq(8,403,3)
+logfc_GLDS120<-logfc_GLDS120[,c(1:7,log)]
 write.csv(logfc_GLDS120,"logfc_GLDS120.csv")
